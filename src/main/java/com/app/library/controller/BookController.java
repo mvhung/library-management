022f4212b35c1e3a5600e2d79fe64be1bb18c404
ptuan123987 <87867738+ptuan123987@users.noreply.github.com>
2023-07-24 23:42:12 +0700

    create book , pagination book page

--- src/main/java/com/app/library/controller/BookController.java
+++ src/main/java/com/app/library/controller/BookController.java
@@ -1,23 +1,51 @@
 package com.app.library.controller;
 
+import com.app.library.dto.BookDto;
+import com.app.library.dto.CategoryDto;
+import com.app.library.service.impl.MapValidationErrorService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import com.app.library.model.Book;
 import com.app.library.service.impl.BookServiceImpl;
 
 @RestController
-@RequestMapping("book")
+@RequestMapping("/api/v1/books")
 public class BookController {
 
     @Autowired
-    BookServiceImpl bookService;
+    BookServiceImpl bookServiceImpl;
+    @Autowired
+    MapValidationErrorService mapValidationErrorService;
     @RequestMapping("{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable("id") int id){
-        return bookService.getBook(id);
+    public ResponseEntity<?> getBookById(@PathVariable("id") int id){
+        return bookServiceImpl.getBook(id);
     }
+    @PostMapping
+    public ResponseEntity<?> addBook(@Valid @RequestBody BookDto dto,
+                                        BindingResult result) {
+        ResponseEntity<?> responseEntity =  mapValidationErrorService.mapValidationFields(result);
+        if(responseEntity != null) {
+            return responseEntity;
+        }
+        return bookServiceImpl.addBook(dto);
+    }
+    @GetMapping("/pages")
+    public ResponseEntity<?> getBooks(
+            @PageableDefault(size = 5, sort ="bookTitle", direction = Sort.Direction.ASC)
+            Pageable pageable){
+        Page<Book> bookPage =  bookServiceImpl.findAll(pageable);
+        return new ResponseEntity<>(bookPage, HttpStatus.OK);
+    }
+
+
 
 }
