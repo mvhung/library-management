6bacd4e841e4c3d0f0058e8841c23dd510e744a3
mvhung <maihung1372002@gmail.com>
2023-08-28 17:09:50 +0700

    fix cors link

--- src/main/java/com/app/library/config/SecurityConfiguration.java
+++ src/main/java/com/app/library/config/SecurityConfiguration.java
@@ -74,7 +74,7 @@ public class SecurityConfiguration {
         config.setAllowCredentials(true);
         config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE"));
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        config.setAllowedOrigins(Collections.singletonList("https://library-management.up.railway.app"));
         source.registerCorsConfiguration("/**", config);
         return source;
     }
