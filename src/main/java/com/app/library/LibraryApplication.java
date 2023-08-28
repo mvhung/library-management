460a005c4a05a9775a92a714f65636767be60a41
mvhung <maihung1372002@gmail.com>
2023-08-28 15:25:58 +0700

    add admin to db

--- src/main/java/com/app/library/LibraryApplication.java
+++ src/main/java/com/app/library/LibraryApplication.java
@@ -24,31 +24,31 @@ public class LibraryApplication {
 	}
 
 
-//	@Bean
-//	public CommandLineRunner commandLineRunner(
-//			AuthenticationService service
-//	) {
-//		return args -> {
-//			var admin1 = RegisterRequest.builder()
-//					.firstName("Admin")
-//					.lastName("Admin")
-//					.username("Admin 1")
-//					.email("admin1@mail.com")
-//					.password("password")
-//					.roleName(RoleName.ADMIN)
-//					.build();
-//			System.out.println("Admin token: " + service.registerAdmin(admin1).getToken());
-//
-//			var admin2 = RegisterRequest.builder()
-//					.firstName("Admin")
-//					.lastName("Admin")
-//					.username("Admin 2")
-//					.email("admin2@mail.com")
-//					.password("password")
-//					.roleName(RoleName.ADMIN)
-//					.build();
-//			System.out.println("Admin token: " + service.registerAdmin(admin2).getToken());
-//		};
-//	}
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin1 = RegisterRequest.builder()
+					.firstName("Admin")
+					.lastName("Admin")
+					.username("Admin 1")
+					.email("admin1@mail.com")
+					.password("password")
+					.roleName(RoleName.ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.registerAdmin(admin1).getToken());
+
+			var admin2 = RegisterRequest.builder()
+					.firstName("Admin")
+					.lastName("Admin")
+					.username("Admin 2")
+					.email("admin2@mail.com")
+					.password("password")
+					.roleName(RoleName.ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.registerAdmin(admin2).getToken());
+		};
+	}
 
 }
